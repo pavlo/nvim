51fc559a65869196d09c059114e2c7540b937d54
Pavlo V. Lysov <pavlikus@gmail.com>
2026-04-19 11:56:26 +0300

    changes tabstops to 2 chars

--- lua/config.lua
+++ lua/config.lua
@@ -1,10 +1,9 @@
---vim.opt.guicursor = "i:block"
 vim.opt.nu = true
 vim.opt.relativenumber = false
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
--- vim.opt.colorcolumn = "80"
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 vim.opt.listchars = "tab: ,multispace:|   ,eol:󰌑"
 vim.opt.winborder = "rounded"
 vim.opt.clipboard = "unnamedplus"
